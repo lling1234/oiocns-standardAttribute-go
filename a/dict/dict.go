af262c38a76a8ad5622fc2a3fb6dcb4e85d3f17d
ling66611 <1289475233@qq.com>
2023-03-21 10:44:59 +0800

    feat：完善字典导入平台

--- a/dict/dict.go
+++ a/dict/dict.go
@@ -12,7 +12,8 @@ import (
 
 func ExcelParse() []model.Dict {
 	// 打开Excel文件
-	f, err := excelize.OpenFile("基础配置-业务字典.xlsx")
+	// f, err := excelize.OpenFile("基础配置-业务字典.xlsx")
+	f, err := excelize.OpenFile("基础配置-业务字典last2.xlsx")
 	if err != nil {
 		model.GglogFile.Info(err)
 		return nil
--- a/dict/postPlat.go
+++ a/dict/postPlat.go
@@ -10,151 +10,90 @@ import (
 	"time"
 )
 
+var token string = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJiZWxvbmdJZCI6IjM4MDY2MzQ1NTQ1NzM0OTYzMyIsImNvbXBhbnlJZCI6IjM4MDY2MzQ1NTQ1NzM0OTYzMyIsImV4cCI6MTY3OTM3MDcyMiwiaWF0IjoxNjc5MzYzNTIyLCJ1c2VySWQiOiIzNTg2MjY1Nzg2MTc0NzA5NzYifQ.6ZZ_6kAM5u7hmpTsFevyvnZGboYgvTOsiY01JUfg-H0"
 var tmpDictID string = ""
 
 func PostBlp(data []model.Dict) {
 	fmt.Println("PostBlp data", data)
-	// var tmpDictCOde string = ""
-	var postDict model.PostDict
+	var currentDictCode string = ""
+	// var postDict model.PostDict
 
 	for i, _ := range data {
-		fmt.Println("for i",i)
+		fmt.Println("for i----", i)
 
-		// 第一行，先插入分类，在插入分类项
-		if i == 0 {
-			// 先插入分类
-			// inster dict start
-			postDict.Module = "thing"
-			postDict.Action = "CreateDict"
-
-			postDict.Params.Name = data[i].DictName
-			postDict.Params.Code = data[i].DictCode
-			postDict.Params.BelongID = "380663455457349633"
-			postDict.Params.Remark = data[i].DictName + " " + data[i].DictCode
-			postDict.Params.SpeciesID = "425706835434147840"
-			postDict.Params.Public = true
-
-			pd, err := json.Marshal(postDict)
-			fmt.Println("err111", err)
-			if err != nil {
-				model.GglogFile.Info(err)
-				return
-			}
-			fmt.Println("PostReqDict start---------------")
-			PostReqDict(pd)
-			fmt.Println("PostReqDict end---------------")
-			// inster dict end
-
+		if currentDictCode != data[i].DictCode {
+			fmt.Println("1111 start")
+			InsertDIct(data[i].DictName, data[i].DictCode)
+			currentDictCode = data[i].DictCode
 			time.Sleep(2000)
-			// 在插入分类项
-			// insert dictItem start
-			// 添加字典分类项
-			var postDictItem model.PostDictItem
-			postDictItem.Module = "thing"
-			postDictItem.Action = "CreateDictItem"
-
-			postDictItem.Params.Name = data[i].DictItemName
-			postDictItem.Params.Value = data[i].DictItemCode
-			postDictItem.Params.BelongID = model.DPD.BelongID
-			postDictItem.Params.Remark = data[i].DictItemName + " " + data[i].DictItemCode
-			postDictItem.Params.DictID = tmpDictID
-
-			pdi, err := json.Marshal(postDictItem)
-			if err != nil {
-				model.GglogFile.Info(err)
-				return
-			}
-			fmt.Println("pdi", pdi)
-
-			PostReqDict(pdi)
-			// insert dictItem end
-			time.Sleep(2000)
-
-			continue
-
-		}
-		// 第二行，判断data[0].DictCode是否等于data[1].DictCode，相等就插入分类项，不相等先插入分类，在插入分类项
-		if data[i+1].DictCode == data[i].DictCode {
-			// 相等就插入分类项
-			// insert dict start
-			// 添加字典分类项
-			var postDictItem model.PostDictItem
-			postDictItem.Module = "thing"
-			postDictItem.Action = "CreateDictItem"
-
-			postDictItem.Params.Name = data[i].DictItemName
-			postDictItem.Params.Value = data[i].DictItemCode
-			postDictItem.Params.BelongID = model.DPD.BelongID
-			postDictItem.Params.Remark = data[i].DictItemName + " " + data[i].DictItemCode
-			postDictItem.Params.DictID = tmpDictID
-
-			pdi, err := json.Marshal(postDictItem)
-			if err != nil {
-				model.GglogFile.Info(err)
-				return
-			}
-			fmt.Println("pdi", pdi)
-
-			PostReqDict(pdi)
-			// insert dict end
-			continue
-
+			fmt.Println("tmpDictID", tmpDictID)
+			fmt.Println("1111 end")
 		} else {
-			// 不相等先插入分类，在插入分类项
-			// 先插入分类
-			// inster dict start
-			postDict.Module = "thing"
-			postDict.Action = "CreateDict"
+			fmt.Println("2222 start")
+			fmt.Println("tmpDictID", tmpDictID)
 
-			postDict.Params.Name = data[i].DictName
-			postDict.Params.Code = data[i].DictCode
-			postDict.Params.BelongID = "380663455457349633"
-			postDict.Params.Remark = data[i].DictName + " " + data[i].DictCode
-			postDict.Params.SpeciesID = "425706835434147840"
-			postDict.Params.Public = true
-
-			pd, err := json.Marshal(postDict)
-			fmt.Println("err111", err)
-			if err != nil {
-				model.GglogFile.Info(err)
-				return
-			}
-			fmt.Println("PostReqDict start---------------")
-			PostReqDict(pd)
-			fmt.Println("PostReqDict end---------------")
-			// inster dict end
-
-			// 再插入分类项
-			// insert dict start
-			// 添加字典分类项
-			var postDictItem model.PostDictItem
-			postDictItem.Module = "thing"
-			postDictItem.Action = "CreateDictItem"
-
-			postDictItem.Params.Name = data[i].DictItemName
-			postDictItem.Params.Value = data[i].DictItemCode
-			postDictItem.Params.BelongID = model.DPD.BelongID
-			postDictItem.Params.Remark = data[i].DictItemName + " " + data[i].DictItemCode
-			postDictItem.Params.DictID = tmpDictID
-
-			pdi, err := json.Marshal(postDictItem)
-			if err != nil {
-				model.GglogFile.Info(err)
-				return
-			}
-			fmt.Println("pdi", pdi)
-
-			PostReqDict(pdi)
-			// insert dict end
+			InsertDIctItem(data[i].DictItemName, data[i].DictItemCode)
+			time.Sleep(2000)
+			fmt.Println("2222 end")
 			continue
-
 		}
-		// ----------------------------------------------
+		fmt.Println("3333 start")
+		fmt.Println("tmpDictID", tmpDictID)
 
+		InsertDIctItem(data[i].DictItemName, data[i].DictItemCode)
+		time.Sleep(2000)
+		fmt.Println("3333 end")
+
+		continue
 	}
 
 }
+func InsertDIct(dictName, dictCode string) {
+	var postDict model.PostDict
 
+	postDict.Module = "thing"
+	postDict.Action = "CreateDict"
+
+	postDict.Params.Name = dictName
+	postDict.Params.Code = dictCode
+	postDict.Params.BelongID = "380663455457349633"
+	postDict.Params.Remark = dictName + " " + dictCode
+	postDict.Params.SpeciesID = "425706835434147840"
+	postDict.Params.Public = true
+
+	pd, err := json.Marshal(postDict)
+	fmt.Println("err111", err)
+	if err != nil {
+		model.GglogFile.Info(err)
+		return
+	}
+	fmt.Println("PostReqDict start---------------")
+	PostReqDict(pd)
+	fmt.Println("PostReqDict end---------------")
+}
+func InsertDIctItem(dictItemName, dictItemCode string) {
+	// insert dictItem start
+	// 添加字典分类项
+	var postDictItem model.PostDictItem
+	postDictItem.Module = "thing"
+	postDictItem.Action = "CreateDictItem"
+
+	postDictItem.Params.Name = dictItemName
+	postDictItem.Params.Value = dictItemCode
+	postDictItem.Params.BelongID = "380663455457349633"
+	postDictItem.Params.Remark = dictItemName + " " + dictItemCode
+	postDictItem.Params.DictID = tmpDictID
+
+	pdi, err := json.Marshal(postDictItem)
+	if err != nil {
+		model.GglogFile.Info(err)
+		return
+	}
+	// fmt.Println("pdi", pdi)
+
+	PostReqDictItem(pdi)
+	// insert dictItem end
+}
 func PostReqDict(data []byte) {
 	fmt.Println("PostReqDict entering")
 	// 发送HTTP POST请求
@@ -164,7 +103,7 @@ func PostReqDict(data []byte) {
 		model.GglogFile.Info(err)
 		return
 	}
-	token := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJiZWxvbmdJZCI6IjM1ODYyNjU3ODYxNzQ3MDk3NiIsImNvbXBhbnlJZCI6IjAiLCJleHAiOjE2NzkzMTM4NzQsImlhdCI6MTY3OTMwNjY3NCwidXNlcklkIjoiMzU4NjI2NTc4NjE3NDcwOTc2In0.3y_y9rUGLTBueD5aSO26-pmmZ8GsZ5VZiUkejFdu0rg"
+
 	req.Header.Set("Authorization", token)
 	req.Header.Set("Content-Type", "application/json")
 
@@ -175,14 +114,60 @@ func PostReqDict(data []byte) {
 		fmt.Println("err11", err)
 
 	}
+	time.Sleep(2000)
 	var result1 model.DictResponseInfoI
 	body, err := ioutil.ReadAll(resp.Body)
 	if err == nil {
-		err = json.Unmarshal(body, &result1)
+		fmt.Println("err111fff111", err)
+	}
+	err = json.Unmarshal(body, &result1)
+	if err == nil {
+		fmt.Println("err111fff222", err)
 	}
 	fmt.Println("result1", result1)
 	fmt.Println("result1.Data.ID", result1.Data.ID)
 	tmpDictID = result1.Data.ID
+	time.Sleep(2000)
+
+	defer resp.Body.Close()
+
+	// 输出响应结果
+	// model.GglogFile.Info(resp.Status)
+}
+
+func PostReqDictItem(data []byte) {
+	fmt.Println("PostReqDict entering")
+	// 发送HTTP POST请求
+	var url string = "http://anyinone.com:800/orginone/kernel/rest/request"
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(data))
+	if err != nil {
+		model.GglogFile.Info(err)
+		return
+	}
+
+	req.Header.Set("Authorization", token)
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		// model.GglogFile.Info(err)
+		fmt.Println("err11", err)
+
+	}
+	time.Sleep(2000)
+	var result1 model.DictResponseInfoI
+	body, err := ioutil.ReadAll(resp.Body)
+	if err == nil {
+		fmt.Println("err111fff111", err)
+	}
+	err = json.Unmarshal(body, &result1)
+	if err == nil {
+		fmt.Println("err111fff222", err)
+	}
+	fmt.Println("result1", result1)
+	fmt.Println("result1.Data.ID", result1.Data.ID)
+	time.Sleep(2000)
 
 	defer resp.Body.Close()
 
